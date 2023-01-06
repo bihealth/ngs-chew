5f6a003e8447eb3cdddf2767b007c42a3e3fd8ac
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2023-01-06 16:01:46 +0100

    feat: interpret samtools idxstats output

--- chew/fingerprint.py
+++ chew/fingerprint.py
@@ -14,6 +14,8 @@ import numpy.typing
 import pysam
 import vcfpy
 
+from chew.common import CHROM_LENS_GRCH37, CHROM_LENS_GRCH38, CHROM_LENS_HG19
+
 #: Key to use for GRCh37 release.
 RELEASE_37 = "GRCh37"
 
@@ -30,65 +32,6 @@ BCFTOOLS_CALL = r"bcftools call -c -Oz -o %(calls)s"
 
 #: Template for creating ``samtools idxstats`` call.
 SAMTOOLS_IDXSTATS = r"samtools idxstats %(input_bam)s"
-
-#: Chromosome lengths in GRCh37.
-CHROM_LENS_GRCH37 = {
-    "1": 249250621,
-    "2": 243199373,
-    "3": 198022430,
-    "4": 191154276,
-    "5": 180915260,
-    "6": 171115067,
-    "7": 159138663,
-    "8": 146364022,
-    "9": 141213431,
-    "10": 135534747,
-    "11": 135006516,
-    "12": 133851895,
-    "13": 115169878,
-    "14": 107349540,
-    "15": 102531392,
-    "16": 90354753,
-    "17": 81195210,
-    "18": 78077248,
-    "19": 59128983,
-    "20": 63025520,
-    "21": 48129895,
-    "22": 51304566,
-    "X": 155270560,
-    "Y": 59373566,
-}
-
-#: Chromosome lengths in hg19.
-CHROM_LENS_HG19 = {f"chr{name}": length for name, length in CHROM_LENS_GRCH37.items()}
-
-#: Chromosome lengths in GRCh38.
-CHROM_LENS_GRCH38 = {
-    "chr1": 248956422,
-    "chr2": 242193529,
-    "chr3": 198295559,
-    "chr4": 190214555,
-    "chr5": 181538259,
-    "chr6": 170805979,
-    "chr7": 159345973,
-    "chr8": 145138636,
-    "chr9": 138394717,
-    "chr10": 133797422,
-    "chr11": 135086622,
-    "chr12": 133275309,
-    "chr13": 114364328,
-    "chr14": 107043718,
-    "chr15": 101991189,
-    "chr16": 90338345,
-    "chr17": 83257441,
-    "chr18": 80373285,
-    "chr19": 58617616,
-    "chr20": 64444167,
-    "chr21": 46709983,
-    "chr22": 50818468,
-    "chrX": 156040895,
-    "chrY": 57227415,
-}
 
 
 class InvalidInputWarning(UserWarning):
@@ -297,6 +240,8 @@ def write_fingerprint(
         sections.append("chrx_fingerprint")
     if chrx_aafs is not None:
         sections.append("chrx_aafs")
+    if samtools_idxstats is not None:
+        sections.append("samtools_idxstats")
     header = np.array(
         [
             "ngs_chew_fingerprint",  # file identifier
