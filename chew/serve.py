3c16484317755320baf890d448f04b55b39447a0
Manuel Holtgrewe <manuel.holtgrewe@bihealth.de>
2023-01-16 21:41:05 +0100

    wip

--- chew/serve.py
+++ chew/serve.py
@@ -15,7 +15,12 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 from tqdm import tqdm
-from chew.common import PedigreeMember, pedigree_member_from_tsv
+from chew.common import (
+    CHROM_LENS_GRCH37,
+    CHROM_LENS_GRCH38,
+    PedigreeMember,
+    pedigree_member_from_tsv,
+)
 
 from chew.stats import (
     SampleStats,
@@ -24,6 +29,58 @@ from chew.stats import (
     extract_header,
     load_fingerprint_all,
 )
+
+
+@attrs.frozen
+class ChromDosage:
+    sample_name: str
+    chr_1: float
+    chr_2: float
+    chr_3: float
+    chr_4: float
+    chr_5: float
+    chr_6: float
+    chr_7: float
+    chr_8: float
+    chr_9: float
+    chr_10: float
+    chr_11: float
+    chr_12: float
+    chr_13: float
+    chr_14: float
+    chr_15: float
+    chr_16: float
+    chr_17: float
+    chr_18: float
+    chr_19: float
+    chr_20: float
+    chr_21: float
+    chr_22: float
+    chr_x: float
+    chr_y: float
+
+
+def compute_chrom_dosages(container) -> ChromDosage:
+    samtools_idxstats = container["samtools_idxstats"].tolist()
+    counts = {}
+    for line in samtools_idxstats.splitlines():
+        arr = line.split("\t")
+        counts[arr[0]] = int(arr[2])
+    total = sum(counts.values())
+
+    def to_key(s: str) -> str:
+        if s.startswith("chr"):
+            return s.replace("chr", "chr_").lower()
+        else:
+            return f"chr_{s}".lower()
+
+    kwargs = {
+        to_key(k): v / total
+        for k, v in counts.items()
+        if k in CHROM_LENS_GRCH37 or k in CHROM_LENS_GRCH38
+    }
+    header = extract_header(container)
+    return ChromDosage(sample_name=header.sample, **kwargs)
 
 
 @attrs.frozen
@@ -45,6 +102,14 @@ def load_all_stats(config: Config):
     for container in map(load_fingerprint_all, tqdm(config.fingerprints)):
         all_stats.append(compute_sample_stats(container))
     return all_stats
+
+
+def load_all_chrom_dosages(config: Config):
+    logger.info("Loading all chrom dosage data")
+    chrom_dosages = []
+    for container in map(load_fingerprint_all, tqdm(config.fingerprints)):
+        chrom_dosages.append(compute_chrom_dosages(container))
+    return chrom_dosages
 
 
 def load_ped_file(config: Config) -> typing.List[PedigreeMember]:
@@ -111,6 +176,44 @@ CONTENT_STYLE = {
 }
 
 
+def build_select_div(
+    field_id: str,
+    label: str,
+    initial_value: str,
+    anno_dims,
+) -> html.Div:
+    return html.Div(
+        [
+            dbc.Select(
+                options=[
+                    {
+                        "label": COLUMN_LABELS.get(dim, dim),
+                        "value": dim,
+                    }
+                    for dim in SCATTER_DIMS + anno_dims
+                ],
+                value=initial_value,
+                id=field_id,
+            ),
+            html.Label(
+                [label],
+                htmlFor=field_id,
+            ),
+        ],
+        className="form-floating col-12",
+    )
+
+
+def unpivot(df, key="key", variable="variable", value="value"):
+    n, k = df.shape
+    data = {
+        value: df.to_numpy().ravel("F"),
+        variable: np.asarray(df.columns).repeat(n),
+        key: np.tile(np.asarray(df.index), k),
+    }
+    return pd.DataFrame(data, columns=[key, variable, value])
+
+
 def run(config: Config):
     logger.info("Running report server...")
     logger.info("")
@@ -118,6 +221,8 @@ def run(config: Config):
     df_stats = pd.DataFrame.from_records(
         list(map(cattrs.unstructure, all_stats)),
     )
+    all_chrom_dosages = load_all_chrom_dosages(config)
+    df_chrom_dosages = pd.DataFrame.from_records(list(map(cattrs.unstructure, all_chrom_dosages)))
     df_stats["sample_name"] = df_stats["sample_name"].str.replace(config.strip_suffix, "")
 
     # Load pedigree
@@ -150,8 +255,8 @@ def run(config: Config):
             dbc.Nav(
                 [
                     dbc.NavLink("Stats Table", href="/", active="exact"),
-                    dbc.NavLink("Stats Plots", href="/stats-plots", active="exact"),
-                    dbc.NavLink("Page 2", href="/page-2", active="exact"),
+                    dbc.NavLink("Stats Plots: X vs. Y", href="/stats-plots", active="exact"),
+                    dbc.NavLink("Dosage per Chrom.", href="/chrom-dosage-plots", active="exact"),
                 ],
                 vertical=True,
                 pills=True,
@@ -210,6 +315,48 @@ def run(config: Config):
         fig.update_traces(marker={"size": int(marker_size)})
         return dcc.Graph(figure=fig)
 
+    @app.callback(
+        Output("chrom-dosage-plots", "children"),
+        [
+            Input("chrom-dosage-select-dim-color", "value"),
+            Input("chrom-dosage-select-dim-symbol", "value"),
+            Input("chrom-dosage-select-marker-size", "value"),
+        ],
+    )
+    def render_chrom_dosage_plot(color, symbol, marker_size):
+        df_chrom_dosage_plot_tmp = unpivot(
+            df_chrom_dosages.set_index("sample_name"),
+            "sample_name",
+            "chrom",
+            "dosage",
+        )
+        df_chrom_dosage_plot_tmp["chrom"] = df_chrom_dosage_plot_tmp["chrom"].str.upper()
+        df_chrom_dosage_plot_tmp["chrom"] = df_chrom_dosage_plot_tmp["chrom"].str.replace("CHR_", "")
+        df_chrom_dosage_plot = (
+            df_ped.set_index("name")
+            .join(df_chrom_dosage_plot_tmp.set_index("sample_name"))
+            .reset_index()
+        )
+        if config.annos_tsv:
+            logger.info("Loading annotations TSV %s", config.annos_tsv)
+            df_anno = pd.read_csv(config.annos_tsv, sep="\t")
+            df_anno_0 = df_anno.columns[0]
+            df_anno[df_anno_0] = df_anno[df_anno_0].str.replace(config.strip_suffix, "")
+            df_chrom_dosage_plot = (
+                df_chrom_dosage_plot.set_index("name")
+                .join(df_anno.set_index(df_anno_0))
+                .reset_index()
+            )
+        fig = px.strip(
+            df_chrom_dosage_plot,
+            x="chrom",
+            y="dosage",
+            color=color,
+            hover_data=["index", "dosage"],
+        )
+        fig.update_traces(marker={"size": int(marker_size)})
+        return dcc.Graph(figure=fig)
+
     @app.callback(Output("page-content", "children"), [Input("url", "pathname")])
     def render_page_content(pathname):
         if pathname == "/":
@@ -232,39 +379,16 @@ def run(config: Config):
                 page_size=20,
             )
         elif pathname == "/stats-plots":
-
-            def build_select_div(
-                field_id: str,
-                label: str,
-                initial_value: str,
-            ) -> html.Div:
-                return html.Div(
-                    [
-                        dbc.Select(
-                            options=[
-                                {
-                                    "label": COLUMN_LABELS.get(dim, dim),
-                                    "value": dim,
-                                }
-                                for dim in SCATTER_DIMS + anno_dims
-                            ],
-                            value=initial_value,
-                            id=field_id,
-                        ),
-                        html.Label(
-                            [label],
-                            htmlFor=field_id,
-                        ),
-                    ],
-                    className="form-floating col-12",
-                )
-
             row_select = html.Form(
                 [
-                    build_select_div("stats-scatter-select-dim-h", "horizontal axis", "sex"),
-                    build_select_div("stats-scatter-select-dim-v", "vertical axis", "chrx_het_hom"),
-                    build_select_div("stats-scatter-select-dim-color", "color", "sex"),
-                    build_select_div("stats-scatter-select-dim-symbol", "shape", "sex"),
+                    build_select_div(
+                        "stats-scatter-select-dim-h", "horizontal axis", "sex", anno_dims
+                    ),
+                    build_select_div(
+                        "stats-scatter-select-dim-v", "vertical axis", "chrx_het_hom", anno_dims
+                    ),
+                    build_select_div("stats-scatter-select-dim-color", "color", "sex", anno_dims),
+                    build_select_div("stats-scatter-select-dim-symbol", "shape", "sex", anno_dims),
                     html.Div(
                         [
                             dbc.Select(
@@ -306,8 +430,52 @@ def run(config: Config):
                     row_plot,
                 ]
             )
-        elif pathname == "/page-2":
-            return html.P("Oh cool, this is page 2!")
+        elif pathname == "/chrom-dosage-plots":
+            row_select = html.Form(
+                [
+                    build_select_div("chrom-dosage-select-dim-color", "color", "sex", anno_dims),
+                    build_select_div("chrom-dosage-select-dim-symbol", "shape", "sex", anno_dims),
+                    html.Div(
+                        [
+                            dbc.Select(
+                                options=[
+                                    {"label": str(2 * i), "value": str(2 * i)} for i in range(1, 11)
+                                ],
+                                value="6",
+                                id="chrom-dosage-select-marker-size",
+                            ),
+                            html.Label(
+                                ["size"],
+                                htmlFor="chrom-dosage-select-marker-size",
+                            ),
+                        ],
+                        className="form-floating col-12",
+                    ),
+                ],
+                className="row row-cols-lg-auto g-3 align-items-center",
+            )
+            row_plot = html.Div(
+                [
+                    html.Div(
+                        [
+                            dbc.Placeholder(xs=6),
+                            html.Br(),
+                            dbc.Placeholder(className="w-75"),
+                            html.Br(),
+                            dbc.Placeholder(style={"width": "25%"}),
+                        ],
+                        id="chrom-dosage-plots",
+                        className="col-12",
+                    )
+                ],
+                className="row",
+            )
+            return html.Div(
+                [
+                    row_select,
+                    row_plot,
+                ]
+            )
         # If the user tries to reach a different page, return a 404 message
         return html.Div(
             [
@@ -318,4 +486,7 @@ def run(config: Config):
             className="p-3 bg-light rounded-3",
         )
 
-    app.run_server()
+    app.run_server(
+        dev_tools_hot_reload=True,
+        debug=True,
+    )
