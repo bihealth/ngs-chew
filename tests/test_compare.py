effd3e75e30c7097f5f11be58454a6947db51d5a
Manuel Holtgrewe <manuel.holtgrewe@bih-charite.de>
2024-01-08 11:00:44 +0100

    wip

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -2,6 +2,8 @@ from chew import compare
 
 
 def test_load_fingerprint(path_tests):
-    header, fingerprint = compare.load_fingerprint(False, str(path_tests / "data/igsr.HG00102.TP73.npz"))
+    header, fingerprint = compare.load_fingerprint(
+        False, str(path_tests / "data/igsr.HG00102.TP73.npz")
+    )
     assert header == "HG00102-N1-DNA1-WES1"
     assert fingerprint.shape == (3, 23770)
